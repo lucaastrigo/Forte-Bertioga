672fbacf9e4ba65c2811006cdccfc42d7a22b514
lucaastrigo <lucaas.trigo@gmail.com>
2021-05-31 12:04:32 -0300

    player movement script created;

---
+++ Forte Bertioga 2021.1/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 mousePos;
+
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        //
+    }
+}
