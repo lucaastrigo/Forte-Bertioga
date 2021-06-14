d20c5499f80f37daa76e246a8687b8e9a951826f
Brunoocm <brunocm64@gmail.com>
2021-06-14 17:35:30 -0300

    NavMesh Movimentacao

--- Forte Bertioga 2021.1/Assets/Scripts/Player/PlayerMovement.cs
+++ Forte Bertioga 2021.1/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
+    NavMeshAgent agent;
+
     Vector3 mousePos;
 
     void Start()
     {
-        //
+        agent = GetComponentInChildren<NavMeshAgent>();
+
     }
 
     void Update()
@@ -26,7 +31,9 @@ public class PlayerMovement : MonoBehaviour
 
             if(Physics.Raycast(ponto, out hit, Mathf.Infinity))
             {
-                if (hit.transform.tag == "Chao") transform.position = hit.point;
+                if (hit.transform.tag == "Chao") agent.SetDestination(hit.point);
+
+                //transform.position = hit.point
 
                 if (hit.transform.gameObject.GetComponent<MiniGameAccess>() != null)
                 {
