64258ea76338cfcb83d13a86e7ae12480c342e1a
Brunoocm <brunocm64@gmail.com>
2021-09-21 22:26:23 -0300

    joystick isometrico

---
+++ Forte Bertioga 2021.1/Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+    public SpriteRenderer sprite;
+    public Animator anim;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+      
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = (Vector3.forward + Vector3.right) * variableJoystick.Vertical + (Vector3.right + Vector3.back) * variableJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        if (direction.z > -1 && direction.z < 1.5f && direction.x < 1)
+        {
+            EsquerdaSprite();
+        }
+        else
+        {
+            DireitaSprite();
+        }
+
+        anim.SetFloat("Horizontal", direction.x);
+        anim.SetFloat("Vertical", direction.z);
+        anim.SetFloat("Speed", direction.sqrMagnitude);
+    }
+
+
+
+    void DireitaSprite()
+    {
+        sprite.flipX = true; //dir
+    }
+    void EsquerdaSprite()
+    {
+        sprite.flipX = false; //esq
+    }
+}
--- Forte Bertioga 2021.1/Assets/_Game/Scripts/Player/PlayerMovement.cs
+++ Forte Bertioga 2021.1/Assets/_Game/Scripts/Player/PlayerMovement.cs
@@ -23,155 +23,20 @@ public class PlayerMovement : MonoBehaviour
     {
         agent = GetComponentInChildren<NavMeshAgent>();
         cam = camera.GetComponent<Camera>();
-
-
     }
 
     void Update()
     {
-        //print(agent.velocity.z + "VELOCIDADE X");
-        //print(agent.velocity.x + "VELOCIDADE X");
-
         if (Input.GetKeyDown(KeyCode.F))
         {
             agent.velocity = Vector3.zero;
             agent.isStopped = true;
         }
 
-        //if (agent.velocity.x > 0.1f && agent.velocity.z < 0.1f)
-        //{
-        //    WalkUp();
-        //    DireitaSprite();
-        //}
-        //else if (agent.velocity.x < -0.1f && agent.velocity.z < 0.1f)
-        //{
-        //    WalkDown();
-        //    EsquerdaSprite();      
-        //}  
-
-        //if (agent.velocity.z > 0.1f && agent.velocity.x < 0.1f)
-        //{
-        //    WalkUp();
-        //    EsquerdaSprite(); 
-        //}
-        //else if (agent.velocity.z < -0.1f && agent.velocity.x < 0.1f)
-        //{
-        //    DireitaSprite();
-        //    WalkDown();
-        //}
-
-
-
-        if (agent.steeringTarget.z > transform.position.z && agent.steeringTarget.x < transform.position.x)
-        {
-            EsquerdaSprite();
-            if (agent.steeringTarget.z > transform.position.z)
-            {
-                WalkUp();
-            }
-            if (agent.steeringTarget.x < transform.position.x)
-            {
-                WalkDown();                
-            }
-        }
-        else if (agent.steeringTarget.z < transform.position.z && agent.steeringTarget.x > transform.position.x)
-        {
-            DireitaSprite();
-            if (agent.steeringTarget.z < transform.position.z)
-            {
-                WalkUp();
-            }
-            if (agent.steeringTarget.x > transform.position.x)
-            {
-                WalkDown();
-            }
-        }
-
-        if (agent.steeringTarget.x > transform.position.x && agent.steeringTarget.z > transform.position.z)
-        {
-            WalkUp();
-            if(agent.velocity.x > 3)
-            {
-                DireitaSprite();
-            }
-            else
-            {
-                EsquerdaSprite();
-            }
-        }
-        else if (agent.steeringTarget.x < transform.position.x && agent.steeringTarget.z < transform.position.z)
-        {
-            WalkDown();
-            if (agent.velocity.z < -3)
-            {
-                DireitaSprite();
-            }
-            else
-            {
-                EsquerdaSprite();
-            }
-        }
-
-
-
-
-        if (agent.velocity == Vector3.zero)
-        {
-            anim.SetBool("walk", false);
-            anim.SetBool("walkUp", false);
-        }
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            //mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-           
-            //transform.position = mousePos;
-
-            Ray ponto = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(Physics.Raycast(ponto, out hit, Mathf.Infinity))
-            {
-                if (hit.transform.tag == "Chao") agent.SetDestination(hit.point);
-
-                //transform.position = hit.point
-
-                if (hit.transform.gameObject.GetComponent<MiniGameAccess>() != null)
-                {
-                    //hit.transform.gameObject.GetComponent<MiniGameAccess>().AccessMinigame();
-                }
-               
-            }
-        }
-
-        //if(zoom & cam.orthographicSize >= 7.5f)
-        //{
-        //    cam.orthographicSize -= Time.deltaTime * speedCamera;
-        //} 
         if(!zoom & cam.orthographicSize <= 15)
         {
             cam.orthographicSize += Time.deltaTime * speedCamera;
         }
-    }
-
-    void WalkDown()
-    {
-        anim.SetBool("walk", true);
-        anim.SetBool("walkUp", false);
-    } 
-    void WalkUp()
-    {
-        anim.SetBool("walkUp", true);
-        anim.SetBool("walk", false);
-    }
-    void DireitaSprite()
-    {
-        sprite.flipX = true; //dir
-    }
-    void EsquerdaSprite()
-    {
-        sprite.flipX = false; //esq
     }
 
 
@@ -203,9 +68,6 @@ public class PlayerMovement : MonoBehaviour
         if (other.gameObject.CompareTag("Room"))
         {
             Room scriptRoom = other.GetComponent<Room>();
-
-
-
 
             if (scriptRoom != null)
             {
